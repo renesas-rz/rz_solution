26978af3369f75b487db64f5828f6abaaf4ee350
Djuned Fernando Djusdek <djunedfernando.djusdek.wz@renesas.com>
2026-07-16 15:26:22 +0900

    rz solution: serve.sh: support auto detect un occupied port
    start from 8000, if in used, continue to find by +1.
    
    Signed-off-by: Djuned Fernando Djusdek <djunedfernando.djusdek.wz@renesas.com>

--- serve.sh
+++ serve.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
 # serve.sh
-# To serve webpage at yours local ip with port=8000
+# To serve webpage at yours local ip with unoccupied port start from 8000
 
 ip=$(hostname -I | awk '{print $1}')
 port=8000
 
+while nc -vz $ip $port &>/dev/null
+do
+    port=$((port + 1))
+done
+
 address=$ip:$port
+
+echo "will serving on http://$address ..."
 
 mkdocs serve -a $address
